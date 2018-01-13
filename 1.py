d60612b5f9d1f073b9627669d8d641e95e46b404
olalatte <olalatte@yandex.ru>
2018-01-13 21:51:17 +0700

    Add files via upload
    Lab 1

---
+++ 1.py
@@ -0,0 +1,15 @@
+import re
+import requests
+
+
+def find_emails(page):
+    page_text = requests.get(page).text
+    all_emails = re.findall(r'[\w.-]+@[\w.-]+', page_text)
+    unique_list = []
+    for i in all_emails:
+        if i not in unique_list:
+            unique_list.append(i)
+    print(unique_list)
+
+
+find_emails('http://www.tsu.ru/help/contacts.php/')
