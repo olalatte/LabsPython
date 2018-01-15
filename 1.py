fba30e21c1b0673297f839b177707efbfe8fe9b4
olalatte <olalatte@yandex.ru>
2018-01-15 21:07:42 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,7 +4,7 @@ import requests
 
 def find_emails(page):
     page_text = requests.get(page).text
-    all_emails = re.findall(r'[\w.-]+@[\w.-]+', page_text)
+    all_emails = re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-]+\.[a-zA-Z0-9_.+-]+', page_text)
     unique_list = []
     for i in all_emails:
         if i not in unique_list:
